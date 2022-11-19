195d9fb8677a833cdfdbc7cf702e902f2a046c0c
Felicitas <felicitas.aguerralde@gmail.com>
2022-11-18 21:59:14 -0300

    componente list viajes creado

--- src/app/viajes-list/viajes-list.component.ts
+++ src/app/viajes-list/viajes-list.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./viajes-list.component.css']
 })
 export class ViajesListComponent {
-
+  viajes={
+    "salida": "Mar del Plata",
+    "destino":"Tandil",
+    "dia": "22/12/2022",
+    "horario":"11:00",
+    "lugares": 2,
+    "mascota":"perro/a",
+    "precio":1200,
+    "images":"assets/img/viaje1.jpg",
+  }
 }
